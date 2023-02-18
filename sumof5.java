664dd38da3b92d939772acda13751593eb164ca7
Sripathi Avinash Kumar <65992275+SreepathiAvinashKumar@users.noreply.github.com>
2023-02-18 21:08:29 +0530

    Update sumof5.java

--- sumof5.java
+++ sumof5.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+
+public class Main
+{
+    public static void main(String[] args) {
+   int a=1;
+   int b=2;
+   int c=3;
+   int d=4;
+   int e=5;
+   int sum;
+   sum = a+b+c+d+e;
+   System.out.println("Sum is "+sum);
+}
+}
