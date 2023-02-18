b9c1df33e7d7aab00a00991f78a99a6f1079add0
Sripathi Avinash Kumar <65992275+SreepathiAvinashKumar@users.noreply.github.com>
2023-02-18 21:03:33 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+
+public class Main
+{
+    public static void main(String[] args) {
+    System.out.println("enter a number");
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    
+    if(a>0){
+        System.out.println("It is a Positive Number");
+    }
+    else{
+        System.out.println("It is a Negative Number");
+    }
+}
+}
